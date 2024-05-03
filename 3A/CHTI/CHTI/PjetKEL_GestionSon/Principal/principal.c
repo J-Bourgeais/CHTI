14ecacd270d3573aaa84088ab4f923e81385843b
Julie Bourgeais <bourgeai@insa-toulouse.fr>
2024-05-03 10:48:27 +0200

    Debut jeu

--- 3A/CHTI/CHTI/PjetKEL_GestionSon/Principal/principal.c
+++ 3A/CHTI/CHTI/PjetKEL_GestionSon/Principal/principal.c
@@ -4,12 +4,22 @@
 #include "ServiceJeuLaser.h"
 #include "DFT.h"
 #include "Signal_4_12.h"
+#include <stdint.h>
 
 extern void GestionSon_callback(void);
 extern int PeriodeSonMicroSec;
 int compteur=0;
 
-//extern (unsigned int)* SortieSon;
+
+short * dma_buf;
+int dft[64];
+void Callback(void){
+		for (int i=0; i<64; i++){
+			int value=DFT(i, dma_buf);
+			dft[i]=value;
+		}
+}
+	
 
 	
 int main(void)
@@ -24,9 +34,22 @@ CLOCK_Configure();
 
 /* Configuration du son (voir ServiceJeuLaser.h) 
  Insérez votre code d'initialisation des parties matérielles gérant le son ....*/	
+	
+	SysTick_Config(5);
+	SysTick(1, Callback);
+	ServJeuLASER_Systick_IT_Init(5, 1, void(Callback*(void));
+
+	
+	ServJeuLASER_ADC_DMA(dma_buf);
+	ServJeuLASER_StartDMA();
+	
+	
 
 ServJeuLASER_Son_Init(PeriodeSonMicroSec, 0, &(GestionSon_callback));
 //PWM_Set_Value_TIM3_Ch3(*SortieSon);
+
+	ServJeuLASER_ADC_DMA(LeSignal);
+	ServJeuLASER_StartDMA();
 	
 Afficher();
 	
