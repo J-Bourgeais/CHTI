bb019273a58d87c44f1550ed9bfab8f4bb4d49d4
Julie Bourgeais <bourgeai@insa-toulouse.fr>
2024-05-28 10:17:57 +0200

    jeu relié (2)

--- 3A/CHTI/CHTI/PjetKEL_GestionSon/Principal/principal.c
+++ 3A/CHTI/CHTI/PjetKEL_GestionSon/Principal/principal.c
@@ -6,24 +6,52 @@
 #include "Signal_4_12.h"
 #include <stdint.h>
 
+
+#define THRESHOLD 0x00030000
+
 extern void GestionSon_callback(void);
+extern void GestionSon_Start(void);
 extern int PeriodeSonMicroSec;
 int compteur=0;
 
 short dma_buf[64];
 int dft[64];
+int Players[6];
+int Scores[6]={0};
+int previousShot[6]={0};
+
 void Callback(void){
 	ServJeuLASER_StartDMA();
-		for (int i=0; i<64; i++){
+		Players[0]=DFT(16, dma_buf)>>32;
+		Players[1]=DFT(17, dma_buf)>>32;
+		Players[2]=DFT(18, dma_buf)>>32;
+		Players[3]=DFT(19, dma_buf)>>32;
+		Players[4]=DFT(23, dma_buf)>>32;
+		Players[5]=DFT(24, dma_buf)>>32;
+	
+		for(int i=0; i<6; i++){
+			if (Players[i]>THRESHOLD){
+				if (previousShot[i]!=1){
+					GestionSon_Start();
+					Scores[i]++;
+				}
+				previousShot[i]=1;
+			} else {
+				previousShot[i]=0;
+			}
+		}
+	
+		/*for (int i=0; i<64; i++){
 			int value=(DFT(i, dma_buf)>>32);
 			dft[i]=value;
-		}
+		}*/
 }
+
 	
 
 	
 int main(void)
-{
+	{
 
 // ===========================================================================
 // ============= INIT PERIPH (faites qu'une seule fois)  =====================
